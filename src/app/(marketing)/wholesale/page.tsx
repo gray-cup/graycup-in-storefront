f3fd23d64b891b8d3751b870fb0ea8261216d0c1
Arjun Aditya <arjunaditya92006@gmail.com>
2026-07-30 21:43:16 +0530

    Update page.tsx

--- src/app/(marketing)/wholesale/page.tsx
+++ src/app/(marketing)/wholesale/page.tsx
@@ -4,10 +4,10 @@ export default function WholesalePage() {
       <div className="max-w-5xl mx-auto px-4 lg:px-6">
         <div className="text-start mb-16">
           <h1 className="text-3xl md:text-4xl font-semibold text-black mb-3">
-            Wholesale
+            Wholesale Indian Roasted Coffee Beans and Ground Coffee
           </h1>
           <p className="text-md md:text-lg text-muted-foreground">
-            Buy Products in Bulk Quantities
+            Buy Indian Roasted Coffee Whole Beans and Ground Coffee in Bulk Quantities
           </p>
         </div>
         </div>
