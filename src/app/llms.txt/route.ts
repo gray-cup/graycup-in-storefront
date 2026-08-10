f33fb6919b3b51535e8425279a2efdbb6c2db175
Arjun Aditya <arjunaditya92006@gmail.com>
2026-08-10 22:09:26 +0530

    iterate

--- src/app/llms.txt/route.ts
+++
@@ -1,8 +0,0 @@
-import { createGET } from "next-llms-generator/route";
-
-export const GET = createGET({
-  generatorOptions: {
-    siteUrl: "http://graycup.in/",
-    enableRecursiveDiscovery: true,
-  },
-});
