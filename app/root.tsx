37ebfcdadd825289df6b19eeb371b2cee0ebbb76
Arjun Aditya <arjunaditya92006@gmail.com>
2026-08-28 21:02:27 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -95,7 +95,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-width=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: dark)" />
         <meta name="color-scheme" content="light" />
